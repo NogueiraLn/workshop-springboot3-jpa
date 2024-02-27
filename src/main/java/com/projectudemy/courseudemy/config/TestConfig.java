b4a7d26eefb4064f3fe1d28ba79d5df9d26b9e62
Nog <lucas.nogueiramartins@gmail.com>
2024-02-26 22:52:32 -0300

    Payment, one-to-one association

--- src/main/java/com/projectudemy/courseudemy/config/TestConfig.java
+++ src/main/java/com/projectudemy/courseudemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectudemy.courseudemy.entities.Category;
 import com.projectudemy.courseudemy.entities.Order;
 import com.projectudemy.courseudemy.entities.OrderItem;
+import com.projectudemy.courseudemy.entities.Payment;
 import com.projectudemy.courseudemy.entities.Product;
 import com.projectudemy.courseudemy.entities.User;
 import com.projectudemy.courseudemy.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
